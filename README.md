934171398c45e161e9e6ee40998ef7e411467958
Maxxxnech <maxxxnech@gmail.com>
2021-12-24 23:27:52 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-vxubqi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-vxubqi)
---
+++ index.js
@@ -0,0 +1,9 @@
+// Import stylesheets
+import './style.css';
+
+
+
+window.onload = function(){
+  const appDiv = document.getElementById('app');
+  //appDiv.innerHTML = `<h1>JS Starter</h1>`;
+}
